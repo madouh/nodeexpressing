602f47e5abbe1740ea5fc193ea642272117b6d6e
madouh <tin.tin.nagi@gmail.com>
2016-01-28 23:50:46 +0200

    This is the nodejs express app

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/info', function(req, res, next) {
+  res.send('Hello from send...');
+});
+router.get('/mydata', function(req, res, next) {
+  res.send('My name : Mamdouh Abbas Salama Farh.');
+});
+router.get('/mydata/:id', function(req, res, next) {
+	var name=['ممدوح','سهير','زهراء','محمد',"أحمد","عمر"]
+	var age=[44,40,15,12,10,7]
+	var x=req.params.id;
+	var y=name[x]
+	var z=age[x]
+  res.send("اسمي: "+y+" و عمري :"+z);
+});
+router.get('/info/:id', function(req, res, next) {
+	var name=['ممدوح','سهير','زهراء','محمد',"أحمد","عمر"]
+	var age=[44,40,15,12,10,7]
+	var x=req.params.id;
+	var y=name[x]
+	var z=age[x]
+  res.render('info',{name:y,age:z});
+});
+
+module.exports = router;
